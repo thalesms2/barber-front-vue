6b9987a07d77daddfc2036d8dcd20d098e5a11b0
Thales Minoru Sato <thalesms2@outlook.com>
2022-08-23 07:47:38 -0300

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+import Header from "./components/Header.vue";
+</script>
+
+<template>
+    <div class="App">
+        <Header />
+    </div>
+</template>
+
+<style scoped>
+.App {
+    color: red;
+}
+</style>
---
+++ src/components/Header.vue
@@ -0,0 +1,30 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+    <div class="navbar">
+        <img src="./../assets/logo.png" alt="logo" class="img-logo">
+        <div>
+            <a href="#">Agende-se</a>
+            <a href="#">Instagram</a>
+            <a href="#">Encontre-nos</a>
+        </div>
+    </div>
+</template>
+
+<style scoped>
+.navbar {
+    background-color: #808080;
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+}
+a {
+    padding: .5em;
+}
+.img-logo {
+    width: 40px;
+    height: 40px;
+}
+</style>
